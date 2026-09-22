e29f3fac0746f634f84d0c9ca9c881799746c467
Antonio Arango <31933285+AntonioBarrios@users.noreply.github.com>
2026-09-22 14:46:31 -0600

    Create article.php

---
+++ app/Models/article.php
@@ -0,0 +1,24 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'slug', 'content', 'author_id', 'category_id', 'is_published', 'views'
+    ];
+
+    // Relaciones POO
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
